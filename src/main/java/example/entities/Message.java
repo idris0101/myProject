b455422fe147d7d75903381452dbe2d875a756cc
idris-rr <idris-rr@mail.ru>
2021-02-28 11:53:49 +0600

    comm

---
+++ src/main/java/example/entities/Message.java
@@ -0,0 +1,72 @@
+package example.entities;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String text;
+    @NotNull
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public Message(Long id, String text, String tag) {
+        this.id = id;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
